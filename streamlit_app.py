1cead4156c21e6f78867161b04d3d1b5601396ab
sriharini1507 <161572534+sriharini1507@users.noreply.github.com>
2026-04-29 13:48:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Smart harini Analyzer", layout="centered")
 
-st.title("💰 Smart Expense Analyzer")
+st.title("💰 Smart harini Expense Analyzer")
 st.write("Analyze your expenses and savings easily!")
 
 # Input section
